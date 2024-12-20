eedb2173846304a2fe113b7b6e5ccaa98eb29fa9
ROJAPONNA-25 <22bk1a7450@stpetershyd.com>
2024-12-20 15:12:36 +0530

    Add files via upload

---
+++ Wonders.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export default function Wonders() {
+        const empData = [
+          { Wonders:"Tajmahal",
+             Location: "Agra",
+             Country: "India",
+             Image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRH6tHSgcUUPFB7Epx276246vA15JuD52PZew&s"},
+          { Wonders:"Greate wall of china", 
+            Location: "China",
+             Country: "China",
+             Image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqklgyLbTFNpYRnPd_uwCNNmnRI50WEfuemg&s"},
+           { Wonders:"Petra",
+             Location: "Jordan", 
+             Country:" Jordan",
+              Image:"  "https://cdn.britannica.com/24/153524-050-BA9D084B/Al-Dayr-Petra-Jordan.jpg"},
+          { Wonders: "The colosseum", 
+            Location: "Rome",
+             country: "Italy", 
+             Image:" https://encrypted-tbn0.gstatic.com/licensed-image?q=tbn:ANd9GcQKs3b2C1dpoRrpIHSQJFQ7JQnWfxf8B9Z0YBcbhA4C7EDywGozBQxn1CCzqn0yIwhoNw3A8IOS3BUR361zpaClr7cGCzexBpTCNZcBq7o},
+          { Wonders: "Chichen itza",
+             Location: "Yucatan",
+              country: "Mexico",
+               Image:"https://lizytravelstheworld.com/wp-content/uploads/2020/06/chichen-itza-el-castillo-pyramid-1024x675.jpg"},
+        
